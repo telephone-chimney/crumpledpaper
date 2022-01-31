7999132341c73d47f8e299be180dca4985498d5b
telephone-chimney <93690661+telephone-chimney@users.noreply.github.com>
2022-01-30 21:00:01 -0500

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,24 @@
+class Ground {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+
+        var options = {
+            isStatic : true,
+        }
+
+        this.body = Bodies.rectangle(x, y, w, h, options)
+        World.add(world, this.body)
+    }
+    
+    display() {
+    push()
+    rectMode(CENTER)
+    fill("yellow")
+    rect(this.body.position.x, this.body.position.y, this.w, this.h)
+    pop()
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+
+var groundObj;
+var leftside;
+var rightside;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1100, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	var ball_options = {
+		isStatic : false,
+		restitution : 0.3,
+		friction : 0,
+		density : 1.2
+	}
+	ball = Bodies.circle(50, 668, 20, ball_options)
+	World.add(world, ball)
+	
+	groundObj = new Ground(width/2, 670, width, 20)
+	leftside = new Ground(1100, 600, 20, 120);
+	rightside = new Ground(350, 600, 20, 120);
+
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ellipse(ball.position.x, ball.position.y, 20)
+  groundObj.display()
+  leftside.display()
+  rightside.display()
+  drawSprites();
+}
+
+function keyPressed() {
+	if(keyCode === UP_ARROW) {
+		Matter.Body.applyForce(ball, ball.position, {x:85, y:-85})
+	}
+}
